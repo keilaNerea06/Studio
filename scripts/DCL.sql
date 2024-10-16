157af1c9f31610191920240df008bb89d05e69fd
Diego-FSP <diegomgplus014@hotmail.com>
2024-10-16 10:04:06 -0300

    TERMINADO 1
    NOSE

--- scripts/DCL.sql
+++ scripts/DCL.sql
@@ -1,12 +1,12 @@
 DROP USER if EXISTS 'director'@'localhost';
-create user 'director'@'localhost' identified by 'passdirector';
-GRANT ALL ON studio.peliculas TO 'director'@'localhost';
-GRANT ALL ON studio.Actor_voz TO 'director'@'localhost';
-GRANT ALL ON studio.Estudio TO 'director'@'localhost';
-GRANT ALL ON studio.Personajes TO 'director'@'localhost';
+create user 'director'@'localhost' identified by 'passDirector12-';
+GRANT SELECT, INSERT, UPDATE, DELETE ON studio.peliculas TO 'director'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON studio.Actor_voz TO 'director'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON studio.Estudio TO 'director'@'localhost';
+GRANT SELECT, INSERT, UPDATE, DELETE ON studio.Personajes TO 'director'@'localhost';
 
 
 DROP USER IF EXISTS 'espectador'@'%';
-CREATE USER 'espectador'@'%' identified by ' ';
+CREATE USER 'espectador'@'%' identified by 'passEspectador12-';
 GRANT SELECT on studio.* to 'espectador'@'%';
 GRANT UPDATE(calificacion) on studio.peliculas to 'espectador'@'%';
